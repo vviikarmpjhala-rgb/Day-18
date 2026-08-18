9a7e4ba607834707acf2d2a6fa4da8c789b96d37
Vviikarm P Jhala <VviikarmPJhala@gmail.com>
2026-08-19 00:10:56 +0530

    fresh code

---
+++ env/dev/terraform.tfvars
@@ -0,0 +1,6 @@
+sdn_rgs = {
+    sdn_rg1 ={
+        name= "sdn_rg1"
+        location="centralindia"
+    }
+}
---
+++ modules/azurerm_resource_group/main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "sdn_rgs" {
+    for_each = var.sdn_rgs
+    name = each.value.name
+    location =each.value.location
+}
